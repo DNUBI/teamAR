d55bb8e7b56c7ad7fa36889c58dc051d44a3e82a
singlecom <51251052+singlecom@users.noreply.github.com>
2019-09-28 13:50:02 +0900

    singlecom
    -AR project 2018 3.14

---
+++ unity/GreatManAR2/Assets/02_Script/GameControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+
+    //ar불러온 타겟이름
+    public string AR_OnTargetString;
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
